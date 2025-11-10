65db9d7411a9823f3405b48c4e72097923a5c2c5
RichTechOFS <richtechlabs@gmail.com>
2025-11-11 02:12:54 +0800

    尝试原始写法v1.02

--- src/lib/utils/rescc.js
+++ src/lib/utils/rescc.js
@@ -1,8 +1,8 @@
+// lib/utils/rescc.js
 export function rescc(res) {
   res.cc = function (message = 'OK', statusCode = 200, data = null) {
-    if (typeof res.status === 'function') res.status(statusCode); // Appwrite & Express 都支持
-
-    res.json({
+    if (typeof res.status === 'function') res.status(statusCode);
+    return res.json({
       success: statusCode >= 200 && statusCode < 300,
       message,
       data,
