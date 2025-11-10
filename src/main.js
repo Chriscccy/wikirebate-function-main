d736e6b3e5323ca6d713b197353956b9ca5acd6e
RichTechOFS <richtechlabs@gmail.com>
2025-11-11 02:40:22 +0800

    尝试原始写法v1.04

--- src/main.js
+++ src/main.js
@@ -1,33 +1,4 @@
-import { ping_handler } from './functions/ping.js';
-import { newUser_handler } from './functions/auth/newUser.js';
-
-// export default async ({ req, res, log }) => {
-//   const body = req.body || {}; // ✅ Appwrite 自动解析好了
-//   log('用原始方法');
-
-//   try {
-//     switch (req.path) {
-//       case '/ping': {
-//         return await ping_handler(req, res, log);
-//       }
-
-//       case '/auth/newUser': {
-//         return await newUser_handler(req, res);
-//       }
-
-//       default:
-//         return res.json({ status: 404, message: 'API Not Found' });
-//     }
-//   } catch (err) {
-//     log('❌ 路由执行失败:', err.message);
-//     return res.json({ error: err.message }, 500);
-//   }
-// };
-
-const routes = {
-  '/ping': ping_handler,
-  '/auth/newUser': newUser_handler,
-};
+import { routes } from './routes/index.js';
 
 export default async ({ req, res, log }) => {
   log('✅ 路由分发开始:', req.path);
