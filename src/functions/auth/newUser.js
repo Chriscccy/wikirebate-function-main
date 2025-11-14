58a26145e8c6cb6cecab8eb8b85b4a2fc983a275
RichTechOFS <richtechlabs@gmail.com>
2025-11-14 22:10:39 +0800

    尝试原始写法v1.27

--- src/functions/auth/newUser.js
+++ src/functions/auth/newUser.js
@@ -1,12 +1,8 @@
-// import { useCurrentUser } from '../../lib/utils/useCurrentUser.js';
 import { createDocument } from '../../lib/utils/createDocument.js';
 import { ID } from 'node-appwrite';
-import { config, client, account } from '../../lib/appwrite.js';
-import { rescc } from '../../lib/utils/rescc.js';
+import { config, account } from '../../lib/appwrite.js';
 
 export async function newUser_handler(req, res, log) {
-  rescc(res);
-
   log('✅ newUser_handler reached');
   try {
     const payload =
@@ -14,18 +10,12 @@ export async function newUser_handler(req, res, log) {
         ? JSON.parse(req.body)
         : req.body || {};
 
-    log('📦 raw body:', req.body);
-    log('📦 parsed payload:', payload);
-    log('📦 password:', payload.password);
-
     const user = await account.create(
       ID.unique(),
       payload.email,
       payload.password,
       payload.name
     );
-
-    // const { userId, email, name } = await useCurrentUser(req);
 
     await createDocument({
       collectionId: config.col.userInfo,
@@ -38,22 +28,11 @@ export async function newUser_handler(req, res, log) {
       },
     });
 
-    log('payload.email:', payload.email);
-
     return {
       status: 200,
       message: 'OK',
-      data: {
-        timestamp: Date.now(),
-        dataPayload: payload,
-        dataReqBody: req.body,
-      },
     };
   } catch (err) {
-    log('❌ error stack:', err.stack);
-    log('❌ error message:', err.message);
-    log('❌ error object:', JSON.stringify(err));
-
     return {
       status: err.statusCode || 500,
       message: err.message || 'Internal Error',
